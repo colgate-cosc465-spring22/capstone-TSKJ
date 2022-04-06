174af0d22e95ccf41d50421401492289b2d25693
Tori Gobo <vgobo@colgate.edu>
2022-04-06 03:05:34 +0000

    worked on reading IPs

---
+++ wiresharkreader.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def main():
+    
+    file = "kwireshark.txt"
+    f=open(file,"r")
+    file = "IPs.txt"
+    outfile = open(file, 'w')
+    lines=f.readlines()
+    counter = 0
+    for x in lines:
+        if(counter != 0):
+            outfile.write(x.split()[3])
+            outfile.write('\n')
+        counter = counter + 1
+    f.close()
+    outfile.close()
+
+if __name__=="__main__":
+    main()
