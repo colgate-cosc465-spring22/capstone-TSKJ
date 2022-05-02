6650b9a8363c446974511240834b04654cd84fa3
Sara <salam@colgate.edu>
2022-05-02 21:32:57 +0000

    got rid of sflow, readnfcapd

---
+++ readnfcap.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# import required module
+import os
+# assign directory
+for n in ['22', '23', '24, '25', '26', '27', '28', '29', '51', '52']:
+    directory = 'sflowData/sflow' + n
+ 
+    # iterate over files in
+    # that directory
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            print(f)
