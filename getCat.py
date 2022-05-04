63536277bb02856e952c6831815938123239e13a
Sara <salam@colgate.edu>
2022-05-04 21:17:58 +0000

    categorized 3 and 5

--- getCat.py
+++ getCat.py
@@ -38,7 +38,7 @@ def getCat(d_name, outf):
     headers = {
                 'Accept': "application/json",
                 'Content-Type': "application/json",
-                'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZWFiY2I0ZTYyMjNmMWZmNGQ1YjcyNWY4OWRhODc4OTMwNjQ0ZjBhOWZkZTUxOTg3MzcyNWJkZmU3NjMzMjQxYjBmZGI2ZGVjYzY3OWUzM2YiLCJpYXQiOjE2NTE2OTc3MjIsIm5iZiI6MTY1MTY5NzcyMiwiZXhwIjoxNjgzMjMzNzIyLCJzdWIiOiI2MTIxIiwic2NvcGVzIjpbXX0.dwHd86bx_8F66Ia2SawHMDz7dkYdP6dG9SA8nO6SoVuTqbzj4sSFy9Z-xZxJ8d2gQo43qflhgnsHK3h5xXbw3w",
+                'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiYjA0NGIzMGFjMmNlZGQ4ZTE3ZGU5OTA0OWYyNjdhNGJkZThjZTQ3MzIwNjI1ZjAwNGVkYmFmMzQzZDdiMDEzMDE4NTFjZWU1Y2QyYmVkM2IiLCJpYXQiOjE2NTE2ODg1ODYsIm5iZiI6MTY1MTY4ODU4NiwiZXhwIjoxNjgzMjI0NTg2LCJzdWIiOiI2MTE4Iiwic2NvcGVzIjpbXX0.o0Em_dd6js5TBBh539DqKKGOhbDqSS7UX8-NUq-3UbtBD5hrZHYUA7dZN-4TK63UJ5A3EwmAwgDL8RI5W7bYvw",
                 'cache-control': "no-cache"
                 }
                 
@@ -65,7 +65,7 @@ def main():
 
     #divide100DNs()  #breaks large file of Domain Names in 100-entry chunks
     
-    fname = "DN7.txt"
+    fname = "DN5.txt"
     f = open(fname,'r')  #replace number after "DN" in fname for the smaller file of DNs to categorize
                          #and use a valid key on line 41
     outf = open(fname[0:3] + "categories.txt", 'a')
